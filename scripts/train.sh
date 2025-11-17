b75c256314bf092eb04afe4b8406cd603751a704
Justin <144951134+Justin034@users.noreply.github.com>
2025-11-16 22:53:11 -0800

    Update train.sh
    Allowed the option for CPU and GPU testing. Make sure to comment out carefully.

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,6 @@
+# Run on 1 GPU
 CUDA_VISIBLE_DEVICES=0 \
 python3 basicsr/train.py -opt options/train/train_ddcolor.yml --auto_resume
+
+# Run on CPU
+# python3 basicsr/train.py -opt options/train/train_ddcolor.yml --auto_resume
